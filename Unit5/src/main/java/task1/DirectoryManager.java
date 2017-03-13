5121c8036eef804d58c8c01520f6c0fb7b48e6a2
SuperflySD <Serdor@mail.ru>
2017-03-13 19:51:23 +0300

    unit 5 task 1

---
+++ Unit5/src/main/java/task1/DirectoryManager.java
@@ -0,0 +1,108 @@
+package task1;
+
+import java.io.*;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class DirectoryManager {
+
+    public List<String> getDirectoryContent(String directoryPath) throws DirectoryNotFoundException {
+        directoryCheck(directoryPath);
+        File path = new File(directoryPath);
+        File[] files = path.listFiles();
+        return Arrays.stream(files).sorted((x, y) -> {
+            if (x.isDirectory() && y.isFile())
+                return -1;
+            if (x.isFile() && y.isDirectory())
+                return 1;
+            else return x.compareTo(y);
+        }).map(x -> {
+            if (x.isFile())
+                return x.getName();
+            if (x.isDirectory())
+                return x.getName() + "(subDirectory)";
+            else return "";
+        }).collect(Collectors.toList());
+    }
+
+    public boolean createFile(String directoryPath, String fileName) throws IOException {
+        directoryCheck(directoryPath);
+        String filePath = directoryPath + "\\" + fileName + ".txt";
+        try {
+            Files.createFile(Paths.get(filePath));
+        } catch (FileAlreadyExistsException e) {
+            System.out.println("File already exists: " + e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    public boolean deleteFile(String directoryPath, String fileName) throws IOException {
+        directoryCheck(directoryPath);
+        String filePath = directoryPath + "\\" + fileName + ".txt";
+        try {
+            Files.delete(Paths.get(filePath));
+        } catch (NoSuchFileException e) {
+            System.out.println("There is no such a file in the directory: " + e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    public String readFile(String directoryPath, String fileName) throws DirectoryNotFoundException {
+        directoryCheck(directoryPath);
+        String filePath = directoryPath + "\\" + fileName + ".txt";
+        StringBuilder str = new StringBuilder();
+        try (FileReader fileReader = new FileReader(filePath)) {
+            BufferedReader bf = new BufferedReader(fileReader);
+            while (true) {
+                String s = bf.readLine();
+                if (s == null)
+                    break;
+                str.append(s + "\n");
+            }
+            bf.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new String(str);
+    }
+
+    public boolean writeToFile(String directoryPath, String fileName, String stringToWrite, boolean destroyPreviousData) throws DirectoryNotFoundException {
+        directoryCheck(directoryPath);
+        String filePath = directoryPath + "\\" + fileName + ".txt";
+        String previousData = "";
+        if (!destroyPreviousData)
+            previousData = readFile(directoryPath, fileName);
+        try (FileWriter fileWriter = new FileWriter(filePath)) {
+            fileWriter.write(previousData + stringToWrite);
+        } catch (IOException e) {
+            System.out.println("Something went wrong during writing to the file...  " + e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    private boolean directoryCheck(String directoryPath) throws DirectoryNotFoundException {
+        if (directoryPath == null)
+            throw new NullPointerException("directoryPath can't be null");
+        File path = new File(directoryPath);
+        if (!path.exists())
+            throw new DirectoryNotFoundException("Pointed directory doesn't exist or unavailable for some other reason");
+        return true;
+    }
+}
+
+
+class DirectoryNotFoundException extends IOException {
+    DirectoryNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ Unit5/src/test/java/task1/DirectoryManagerTest.java
@@ -0,0 +1,50 @@
+package task1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+public class DirectoryManagerTest {
+    private DirectoryManager manager = new DirectoryManager();
+    private final String DIRECTORYPATH = "src\\main\\resources";
+    private final String FILENAME = "newFile";
+
+        @Test
+    public void getDirectoryContent() throws Exception {
+        assertTrue(manager.getDirectoryContent(DIRECTORYPATH).contains("1.txt"));
+
+    }
+    @Test
+    public void createFile() throws Exception {
+        if (manager.getDirectoryContent(DIRECTORYPATH).contains(FILENAME+".txt"))
+            manager.deleteFile(DIRECTORYPATH, FILENAME);
+        assertTrue(manager.createFile(DIRECTORYPATH, FILENAME));
+        assertTrue(manager.getDirectoryContent(DIRECTORYPATH).contains(FILENAME+".txt"));
+    }
+    @Test
+    public void deleteFile() throws Exception {
+        if (!manager.getDirectoryContent(DIRECTORYPATH).contains(FILENAME+".txt"))
+            manager.createFile(DIRECTORYPATH, FILENAME);
+        assertTrue(manager.deleteFile(DIRECTORYPATH, FILENAME));
+        assertFalse(manager.getDirectoryContent(DIRECTORYPATH).contains(FILENAME+".txt"));
+
+    }
+    @Test(expected = DirectoryNotFoundException.class)
+    public void checkForDirectoryNotFoundException() throws Exception {
+        manager.getDirectoryContent(DIRECTORYPATH+1);
+    }
+
+    @Test
+    public void readFile() throws Exception {
+        System.out.println(manager.readFile(DIRECTORYPATH,FILENAME));
+
+    }
+
+    @Test
+    public void writeToFile() throws Exception {
+        manager.writeToFile(DIRECTORYPATH, FILENAME, "ssd Something1", true );
+
+    }
+
+
+}
