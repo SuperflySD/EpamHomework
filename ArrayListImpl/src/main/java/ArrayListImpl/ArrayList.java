8e6b70482a918f7ad7f6c13ec75bd4e906792377
SuperflySD <Serdor@mail.ru>
2017-04-09 15:49:44 +0300

    ArrayList list is done

---
+++ ArrayListImpl/src/main/java/ArrayListImpl/ArrayList.java
@@ -0,0 +1,478 @@
+package ArrayListImpl;
+
+import java.util.*;
+
+public class ArrayList<T> implements List<T> {
+    private int capacity = 10;
+    private T[] data = (T[]) new Object[capacity];
+    private int size = 0;
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        for (int i = 0; i < this.size; i++)
+            if (this.data[i].equals(o))
+                return true;
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        if (c == null)
+            throw new NullPointerException("Input collection can't be null");
+        for (Object i : c)
+            if (!(this.contains(i)))
+                return false;
+        return true;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return Arrays.copyOf(this.data, this.size);
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        if (a == null)
+            throw new NullPointerException("Input array can't be null");
+
+        for (int i = 0; i < Math.min(this.size, a.length); i++)
+            a[i] = (T) this.data[i];
+        return a;
+    }
+
+    @Override
+    public boolean add(T value) {
+        this.checkAndChangeCapacity(1);
+
+        this.data[this.size] = value;
+        size++;
+        return true;
+    }
+
+    @Override
+    public void add(int index, T element) {
+        this.checkAndChangeCapacity(1);
+
+        System.arraycopy(this.data, index, this.data, index + 1, this.size - index);
+        this.data[index] = element;
+        this.size++;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        if (c == null)
+            throw new NullPointerException("Input collection can't be null");
+        for (T i : c)
+            this.add(i);
+
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        if (c == null)
+            throw new NullPointerException("Input collection can't be null");
+        if (index > this.size || index < 0)
+            throw new IndexOutOfBoundsException("Suggesting index is more than number of elements in the linked list or less than zero");
+
+        this.checkAndChangeCapacity(c.size());
+        System.arraycopy(this.data, index, this.data, index + c.size(), this.size - index);
+        int i = index;
+        for (T val : c)
+            this.data[i++] = val;
+
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        if (this.size == 0)
+            return false;
+        if (!(o.getClass().isInstance(this.data[0])))
+            throw new ClassCastException("Passing parameter type doesn't correspond to the parametrized type of the list");
+
+        for (int i = 0; i < this.size; i++)
+            if (this.data[i].equals(o)) {
+                System.arraycopy(this.data, i + 1, this.data, i, this.size - i);
+                this.size--;
+                return true;
+            }
+        return false;
+    }
+
+    @Override
+    public T remove(int index) {
+        if (index < 0 || index > this.size - 1)
+            throw new IndexOutOfBoundsException("Suggesting index is more than number of elements in the list or less than zero");
+
+        T temp = this.data[index];
+        System.arraycopy(this.data, index + 1, this.data, index, this.size - index);
+        this.size--;
+        return temp;
+    }
+
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        if (c == null)
+            throw new NullPointerException("Input collection can't be null");
+        boolean flag = false;
+        for (Object o : c)
+            if (this.remove(o))
+                flag = true;
+        return flag;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        if (c == null)
+            throw new NullPointerException("Input collection can't be null");
+        boolean flag = false;
+        for (T val : this)
+            if (!(c.contains(val))) {
+                this.remove(val);
+                flag = true;
+            }
+        return flag;
+    }
+
+    @Override
+    public void clear() {
+        this.size = 0;
+        this.data = Arrays.copyOf(this.data, Math.max(this.capacity, this.size));
+    }
+
+    public void trimToSize() {
+        this.data = Arrays.copyOf(this.data, this.size);
+    }
+
+    @Override
+    public T get(int index) {
+        if (index < 0 || index > this.size - 1)
+            throw new IndexOutOfBoundsException("Suggesting index is more than number of elements in the list or less than zero");
+
+        return this.data[index];
+    }
+
+    @Override
+    public T set(int index, T element) {
+        if (index < 0 || index > this.size - 1)
+            throw new IndexOutOfBoundsException("Suggesting index is more than number of elements in the list or less than zero");
+        T temp = this.data[index];
+        this.data[index] = element;
+        return temp;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        int i = 0;
+        for (T val : this) {
+            if (val.equals(o))
+                return i;
+            i++;
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        for (int i = this.size - 1; i >= 0; i--)
+            if (this.data[i].equals(o))
+                return i;
+
+        return -1;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new ALIterator<>();
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return new AListIterator<>();
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        return new AListIterator<>(index);
+    }
+
+    @Override
+    public List<T> subList(int fromIndex, int toIndex) {
+        return new SubList(fromIndex, toIndex);
+    }
+
+    private void checkAndChangeCapacity(int numAdd) {
+        if ((this.size + numAdd) * 100 / this.capacity > 80)
+            this.data = Arrays.copyOf(this.data, (this.size + numAdd) * 3 / 2);
+    }
+
+    public class ALIterator<E extends T> implements Iterator<T> {
+        private int curPosition = -1;
+
+        @Override
+        public boolean hasNext() {
+            return this.curPosition < ArrayList.this.size - 1 && ArrayList.this.size != 0;
+        }
+
+        @Override
+        public T next() {
+            if (this.curPosition + 1 >= ArrayList.this.size)
+                throw new IndexOutOfBoundsException("Next call tried to access to a nonexistent element");
+            this.curPosition++;
+            return ArrayList.this.data[this.curPosition];
+        }
+
+        @Override
+        public void remove() {
+            if (this.curPosition < 0)
+                throw new IllegalStateException("You better call next before deleting");
+
+            ArrayList.this.remove(curPosition);
+        }
+    }
+
+    public class AListIterator<E extends T> implements ListIterator<T> {
+        int curPosition = -1;
+
+        @Override
+        public boolean hasNext() {
+            return this.curPosition < ArrayList.this.size - 1 && ArrayList.this.size != 0;
+        }
+
+        @Override
+        public T next() {
+            if (this.curPosition + 1 >= ArrayList.this.size)
+                throw new IndexOutOfBoundsException("Next call tried to access to a nonexistent element");
+            this.curPosition++;
+            return ArrayList.this.data[this.curPosition];
+        }
+
+        @Override
+        public boolean hasPrevious() {
+            return this.curPosition > 0 && ArrayList.this.size != 0;
+        }
+
+        @Override
+        public T previous() {
+            if (this.curPosition - 1 < 0)
+                throw new IndexOutOfBoundsException("Previous call tried to access to a nonexistent element");
+            this.curPosition--;
+            return ArrayList.this.data[this.curPosition];
+        }
+
+        @Override
+        public int nextIndex() {
+            return this.curPosition + 1;
+        }
+
+        @Override
+        public int previousIndex() {
+            return (this.curPosition == 0 || this.curPosition == -1) ? -1 : curPosition - 1;
+        }
+
+        @Override
+        public void remove() {
+            if (this.curPosition < 0)
+                throw new IllegalStateException("You better call next before deleting");
+
+            ArrayList.this.remove(curPosition);
+        }
+
+        @Override
+        public void set(T value) {
+            if (this.curPosition < 0)
+                throw new IllegalStateException("You better call next before setting");
+            ArrayList.this.data[this.curPosition] = value;
+        }
+
+        @Override
+        public void add(T value) {
+            ArrayList.this.add(curPosition + 1, value);
+        }
+
+        public AListIterator() {
+        }
+
+        public AListIterator(int index) {
+            if (index < 0 || index > ArrayList.this.size - 1)
+                throw new IndexOutOfBoundsException("Suggesting index is more than number of elements in the linked list or less than zero");
+
+            this.curPosition = index;
+        }
+
+
+    }
+
+    public class SubList<E extends T> extends ArrayList<T> {
+        private int size;
+        private int start;
+        private int end;
+
+
+        private SubList(int start, int end) {
+            this.size = end - start;
+            this.start = start;
+            this.end = end;
+        }
+
+        @Override
+        public int size() {
+            return this.size;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return this.size == 0;
+        }
+
+        @Override
+        public boolean contains(Object o) {
+            for (int i = start; i < this.end; i++)
+                if (ArrayList.this.data[i].equals(o))
+                    return true;
+            return false;
+        }
+
+        @Override
+        public boolean containsAll(Collection<?> c) {
+            if (c == null)
+                throw new NullPointerException("Input collection can't be null");
+            for (Object i : c)
+                if (!(this.contains(i)))
+                    return false;
+            return true;
+        }
+
+        @Override
+        public Object[] toArray() {
+            return Arrays.copyOfRange(ArrayList.this.data, this.start, this.end);
+        }
+
+        @Override
+        public <T> T[] toArray(T[] a) {
+            if (a == null)
+                throw new NullPointerException("Input array can't be null");
+
+            for (int i = start; i < Math.min(this.end, a.length+start); i++)
+                a[i] = (T) ArrayList.this.data[i];
+            return a;
+        }
+
+        @Override
+        public T get(int index) {
+            if (index < 0 || index > this.size - 1)
+                throw new IndexOutOfBoundsException("Suggesting index is more than number of elements in the list or less than zero");
+
+            return ArrayList.this.data[index+start];
+        }
+
+        @Override
+        public T set(int index, T element) {
+            if (index < 0 || index > this.size - 1)
+                throw new IndexOutOfBoundsException("Suggesting index is more than number of elements in the list or less than zero");
+            T temp = ArrayList.this.data[index+start];
+            ArrayList.this.data[index+start] = element;
+            return temp;
+        }
+
+        @Override
+        public int indexOf(Object o) {
+            for (int i = this.start; i < this.end; i++)
+                if (ArrayList.this.data[i].equals(o))
+                    return i-this.start;
+
+            return -1;
+        }
+
+        @Override
+        public int lastIndexOf(Object o) {
+            for (int i = this.end - 1; i >= this.start; i--)
+                if (ArrayList.this.data[i].equals(o))
+                    return i-start;
+
+            return -1;
+        }
+
+        @Override
+        public boolean add(Object value) {
+            throw new UnsupportedOperationException("Changing of sublist isn't permitted");
+        }
+
+        @Override
+        public void add(int index, Object element) {
+            throw new UnsupportedOperationException("Changing of sublist isn't permitted");
+        }
+
+        @Override
+        public boolean addAll(Collection c) {
+            throw new UnsupportedOperationException("Changing of sublist isn't permitted");
+        }
+
+        @Override
+        public boolean addAll(int index, Collection c) {
+            throw new UnsupportedOperationException("Changing of sublist isn't permitted");
+        }
+
+        @Override
+        public boolean remove(Object o) {
+            throw new UnsupportedOperationException("Changing of sublist isn't permitted");
+        }
+
+        @Override
+        public T remove(int index) {
+            throw new UnsupportedOperationException("Changing of sublist isn't permitted");
+        }
+
+        @Override
+        public boolean removeAll(Collection c) {
+            throw new UnsupportedOperationException("Changing of sublist isn't permitted");
+        }
+
+        @Override
+        public boolean retainAll(Collection c) {
+            throw new UnsupportedOperationException("Changing of sublist isn't permitted");
+        }
+
+        @Override
+        public void clear() {
+            throw new UnsupportedOperationException("Changing of sublist isn't permitted");
+        }
+
+        @Override
+        public void trimToSize() {
+            throw new UnsupportedOperationException("trim to size isn't permitted");
+        }
+
+        @Override
+        public Iterator<T> iterator() {
+            throw new UnsupportedOperationException("Iterator of sublist isn't permitted");
+        }
+
+        @Override
+        public ListIterator<T> listIterator() {
+            throw new UnsupportedOperationException("Iterator of sublist isn't permitted");
+        }
+
+        @Override
+        public ListIterator<T> listIterator(int index) {
+            throw new UnsupportedOperationException("Iterator of sublist isn't permitted");
+        }
+
+        @Override
+        public List<T> subList(int fromIndex, int toIndex) {
+            throw new UnsupportedOperationException("SubList of sublist isn't permitted");
+        }
+    }
+}
---
+++ ArrayListImpl/src/test/java/ArrayListImpl/ArrayListTest.java
@@ -0,0 +1,279 @@
+package ArrayListImpl;
+
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+public class ArrayListTest {
+    ArrayList<Integer> arrayList = new ArrayList<>();
+
+    {
+        for (int i = 0; i < 5; i++)
+            arrayList.add(i);
+    }
+
+    @Test
+    public void size() throws Exception {
+        assertTrue(arrayList.size() == 5);
+    }
+
+    @Test
+    public void isEmpty() throws Exception {
+        assertFalse(arrayList.isEmpty());
+        arrayList.clear();
+        //  assertTrue(arrayList.isEmpty());
+    }
+
+    @Test
+    public void contains() throws Exception {
+        assertTrue(arrayList.contains(0));
+        assertTrue(arrayList.contains(4));
+        assertFalse(arrayList.contains(5));
+    }
+
+    @Test
+    public void containsAll() throws Exception {
+        List<Integer> list = new java.util.ArrayList<>();
+        list.add(1);
+        list.add(3);
+        assertTrue(arrayList.containsAll(list));
+        list.add(9);
+        assertFalse(arrayList.containsAll(list));
+    }
+
+    @Test
+    public void toArrayObjects() throws Exception {
+        Object[] arr = arrayList.toArray();
+        int j = 0;
+        // for (Object i : arr)
+        // assertTrue(arr[j++].equals(i));
+    }
+
+    @Test
+    public void toArrayWithType() throws Exception {
+        Integer[] arr = new Integer[7];
+        arr = arrayList.toArray(arr);
+        // for (int i=0; i<arrayList.size(); i++)
+        ///   assertTrue(arr[i].equals(arrayList[i]));
+    }
+
+    @Test
+    public void add() throws Exception {
+        arrayList.add(7);
+        assertTrue(arrayList.contains(7));
+    }
+
+    @Test
+    public void addWithIndex() throws Exception {
+        arrayList.add(1, 77);
+        assertEquals((int) arrayList.get(0), 0);
+        assertEquals((int) arrayList.get(1), 77);
+        assertEquals((int) arrayList.get(2), 1);
+    }
+
+    @Test
+    public void addAll() throws Exception {
+        List<Integer> list = new ArrayList<>();
+        list.add(5);
+        list.add(6);
+        arrayList.addAll(list);
+        assertTrue(arrayList.containsAll(list));
+    }
+
+    @Test
+    public void addAllWithIndex() throws Exception {
+        List<Integer> list = new ArrayList<>();
+        list.add(6);
+        list.add(7);
+        arrayList.addAll(1, list);
+        assertEquals((int) arrayList.get(0), 0);
+        assertEquals((int) arrayList.get(1), 6);
+        assertEquals((int) arrayList.get(3), 1);
+    }
+
+    @Test
+    public void remove() throws Exception {
+        arrayList.add(7);
+        arrayList.add(8);
+        assertTrue(arrayList.contains(7));
+        assertTrue(arrayList.remove((Object) 7));
+        assertFalse(arrayList.contains(7));
+        assertTrue(arrayList.contains(8));
+    }
+
+    @Test
+    public void removeWithIndex() throws Exception {
+        assertEquals((int) arrayList.remove(1), 1);
+        assertEquals((int) arrayList.get(0), 0);
+        assertEquals((int) arrayList.get(1), 2);
+    }
+
+    @Test(expected = ClassCastException.class)
+    public void removeWithWrongType() throws Exception {
+        arrayList.remove("str");
+    }
+
+    @Test
+    public void removeAll() throws Exception {
+        assertTrue(arrayList.removeAll(Arrays.asList(2, 3)));
+        assertTrue(arrayList.contains(4));
+        assertFalse(arrayList.contains(2));
+    }
+
+    @Test
+    public void retainAll() throws Exception {
+        assertFalse(arrayList.retainAll(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)));
+        assertTrue(arrayList.retainAll(Arrays.asList(0, 1, 9, 8, 7)));
+        assertTrue(arrayList.contains(1));
+        assertFalse(arrayList.contains(2));
+    }
+
+
+    @Test
+    public void clear() throws Exception {
+        arrayList.clear();
+        assertFalse(arrayList.contains(0));
+        assertEquals(arrayList.size(), 0);
+    }
+
+    @Test
+    public void get() throws Exception {
+        int i = 0;
+        for (int val : arrayList) {
+            assertEquals((int) arrayList.get(i), val);
+            i++;
+        }
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void getWithWrongIndex() throws Exception {
+        arrayList.get(5);
+    }
+
+    @Test
+    public void set() throws Exception {
+        assertEquals((int) arrayList.set(0, 55), 0);
+        assertEquals((int) arrayList.get(0), 55);
+    }
+
+    @Test
+    public void indexOf() throws Exception {
+        assertEquals(arrayList.indexOf(1), 1);
+        assertEquals(arrayList.indexOf(99), -1);
+        assertEquals(arrayList.indexOf("str"), -1);
+        assertEquals(arrayList.indexOf(null), -1);
+    }
+
+    @Test
+    public void lastIndexOf() throws Exception {
+        arrayList.add(0, 4);
+        assertEquals(arrayList.lastIndexOf(4), 5);
+        arrayList.remove(5);
+        assertEquals(arrayList.lastIndexOf(4), 0);
+    }
+
+    @Test
+    public void iterator() throws Exception {
+        Iterator<Integer> iterator = arrayList.iterator();
+        for (int i = 0; iterator.hasNext(); i++)
+            assertTrue(iterator.next().equals(i));
+        int j = 0;
+        for (int i : arrayList)
+            assertTrue(i == j++);
+    }
+
+    @Test
+    public void iteratorRemove() throws Exception {
+        Iterator<Integer> iterator = arrayList.iterator();
+        iterator.next();
+        iterator.next();
+        iterator.remove();
+        assertEquals((int) arrayList.get(0), 0);
+        assertEquals((int) arrayList.get(1), 2);
+    }
+
+    @Test
+    public void listIterator() throws Exception {
+        ListIterator<Integer> listIterator = arrayList.listIterator();
+        for (int i = 0; listIterator.hasNext(); i++)
+            assertEquals((int) listIterator.next(), i);
+        for (int i = 3; listIterator.hasPrevious(); i--)
+            assertEquals((int) listIterator.previous(), i);
+    }
+
+    @Test
+    public void listIteratorRemove() throws Exception {
+        ListIterator<Integer> listIterator = arrayList.listIterator();
+        listIterator.next();
+        listIterator.next();
+        listIterator.remove();
+        assertEquals((int) arrayList.get(0), 0);
+        assertEquals((int) arrayList.get(1), 2);
+    }
+
+    @Test
+    public void listIteratorSet() throws Exception {
+        ListIterator<Integer> listIterator = arrayList.listIterator();
+        listIterator.next();
+        listIterator.next();
+        listIterator.set(77);
+        assertEquals((int) arrayList.get(0), 0);
+        assertEquals((int) arrayList.get(1), 77);
+    }
+
+    @Test
+    public void listIteratorAdd() throws Exception {
+        ListIterator<Integer> listIterator = arrayList.listIterator();
+        listIterator.next();
+        listIterator.add(88);
+        assertEquals((int) arrayList.get(0), 0);
+        assertEquals((int) arrayList.get(1), 88);
+    }
+
+    @Test
+    public void listIteratorWithIndex() throws Exception {
+        ListIterator<Integer> listIterator = arrayList.listIterator(1);
+        assertEquals((int) listIterator.next(), 2);
+    }
+
+    @Test
+    public void subList() throws Exception {
+
+        List<Integer> subList = arrayList.subList(1, 3);
+        assertEquals((int) subList.get(0), 1);
+        assertEquals((int) subList.get(1), 2);
+        assertEquals(subList.size(), 2);
+        subList.set(0, 77);
+        assertEquals((int) arrayList.get(1), 77);
+        arrayList.set(1, 99);
+        assertEquals((int) subList.get(0), 99);
+    }
+
+    @Test
+    public void subListContains() throws Exception {
+        List<Integer> subList = arrayList.subList(1, 3);
+        assertTrue(subList.contains(1));
+        assertFalse(subList.contains(0));
+        assertFalse(subList.contains(3));
+    }
+
+    @Test
+    public void subListLastIndexOf() throws Exception {
+        arrayList.add(7);
+        arrayList.add(1,7);
+        List<Integer> subList = arrayList.subList(1, 7);
+        assertEquals(subList.lastIndexOf(7), 5);
+    }
+
+    @Test
+    public void subListIteratorDoesntGoToMainList() throws Exception {
+        List<Integer> subList = arrayList.subList(1, 3);
+        int i = 0;
+        for (int val : subList)
+            i++;
+        assertEquals(i, 2);
+    }
+
+}
